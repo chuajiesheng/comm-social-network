ebd0f75a84be41a041a2a437da5721dc2ccd0435
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-10-10 15:49:45 +0800

    Able to output json and html file

--- graph/how_the_network_look.py
+++ graph/how_the_network_look.py
@@ -3,14 +3,15 @@ import glob
 import os
 import csv
 import json
+import copy
 
 folder = 'input'
-file_type = '*.csv'
 output_folder = 'output'
+file_type = '*.csv'
 
 
-class Student:
-    id = None
+class Survey:
+
     conversed_with = []
     met_facetoface = []
     knowledge_source = []
@@ -20,16 +21,56 @@ class Student:
     trust = []
     attributes = [conversed_with, met_facetoface, knowledge_source, personal_advice, hangout, goto_in_trouble, trust]
 
-    def __init__(self, id):
-        self.id = id
+    def __init__(self):
+        pass
 
-    def add_response(self, question, to, strength):
+    def parse_row(self, row):
+        source_id = row['Q1']
+
+        for k in row.keys():
+            key_parts = k.split('_')
+
+            if len(key_parts) != 3:
+                # need to be in Q<question_id>_<person_id>_Rank
+                continue
+
+            if not row[k].strip():
+                # ignore empty cell
+                continue
+
+            if key_parts[2] == 'Group':
+                # columns with Group does not provide value
+                continue
+
+            question_id = int(key_parts[0][1:])
+            if question_id == 9:
+                # due to a bug in the creation of the questionaire
+                question_id = 8
+
+            destination_id = int(key_parts[1])
+            print('## Adding response: from {}, to {}, for {}, with {}'.format(source_id, destination_id, question_id, row[k]))
+            # question_id - 1 because Q1 is used to denote the person answering the question
+            self.add_response(question_id - 1, source_id, destination_id, int(row[k]))
+
+    def add_response(self, question, source, destination, strength):
         # question - 1 due to indexing
-        self.attributes[question - 1].append((to, strength))
+        # format: {source: "Microsoft", target: "Amazon", type: "relationship", strength: 1}
+        record = {"source": "student_{}".format(source), "target": "student_{}".format(destination),
+                  "type": "relationship", "strength": strength}
+        self.attributes[question - 1].append(record)
+
+    def to_json(self, base_filename):
+        for question_id, response in enumerate(self.attributes):
+            dest = output_folder + os.sep + base_filename + '_Q' + str(question_id + 2) + '.json'
+            print('# Output to: {}'.format(dest))
+            with open(dest, 'w') as out:
+                out.write('var links = ')
+                json.dump(response, out)
+                out.write(';')
 
 
-def find_all_csv(folder):
-    search_path = folder + os.sep + file_type
+def find_all_csv(search_path):
+    search_path = search_path + os.sep + file_type
     print('# Searching: {}'.format(search_path))
 
     f = glob.glob(search_path)
@@ -38,64 +79,51 @@ def find_all_csv(folder):
     return f
 
 
-def parse_row(student, row):
-    for k in row.keys():
-        key_parts = k.split('_')
-
-        if len(key_parts) != 3:
-            # need to be in Q<question_id>_<person_id>_Rank
-            continue
-
-        if not row[k].strip():
-            # ignore empty cell
-            continue
-
-        if key_parts[2] == 'Group':
-            # columns with Group does not provide value
-            continue
-
-        question_id = int(key_parts[0][1:])
-        if question_id == 9:
-            # due to a bug in the creation of the questionaire
-            question_id = 8
-
-        student_id = int(key_parts[1])
-        print('## Adding response: from {}, to {}, for {}, with {}'.format(student.id, student_id, question_id, row[k]))
-        # question_id - 1 because Q1 is used to denote the person answering the question
-        student.add_response(question_id - 1, student_id, int(row[k]))
-
-
 def read_csv(filename):
-    students = dict()
+    survey = Survey()
 
     with open(filename, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            id = row['Q1']
-
-            if not id.strip():
+            if not row['Q1'].strip():
                 continue
 
-            student = Student(id)
-            parse_row(student, row)
+            survey.parse_row(row)
 
-            students[id] = student
-
-    return students
+    return survey
 
 
-def output_json(filename, students):
-    output_file = output_folder + os.sep + filename + '.json'
-    print('# Output to: {}'.format(output_file))
+def output_json(filename, survey):
+    survey.to_json(filename)
+
+
+def copy_base_html(base_filename, survey):
+    f = open('base/base.html', 'r')
+    base_file_data = f.read()
+    f.close()
+
+    no_of_qns = len(survey.attributes)
+    for question_id in range(2, 2 + no_of_qns):
+        json_file = base_filename + '_Q' + str(question_id) + '.json'
+        dest_filename = output_folder + os.sep + base_filename + '_Q' + str(question_id) + '.html'
+
+        html = copy.deepcopy(base_file_data)
+        html = html.replace("data.json", json_file)
+
+        print('# Output to: {}'.format(dest_filename))
+        f = open(dest_filename, 'w')
+        f.write(html)
+        f.close()
 
 
 def main():
     data_files = find_all_csv(folder)
     for f in data_files:
         print('Reading: {}'.format(f))
-        students = read_csv(f)
+        survey = read_csv(f)
         filename = f[len(folder + os.sep):-len('.csv')]
-        output_json(filename, students)
+        output_json(filename, survey)
+        copy_base_html(filename, survey)
 
 
 if __name__ == '__main__':
